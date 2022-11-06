fd38896ecb745e9a665c11fb65bca0e25fb0a543
pdcourses <freshitpt@gmail.com>
2022-11-06 11:58:41 +0200

    ws. client with native methods

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+const { Component } = require("react");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socket = new WebSocket('ws://localhost:3000/');
+
+socket.onopen = function (event){
+  console.dir(event);
+  alert('WS-Connection is opened!');
+}
+
+socket.onclose = function (event){
+  console.dir(event);
+  alert('WS-Connection is closed!');
+}
+
+socket.onmessage = function(event){
+  console.dir(event.data);
+}
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      //messages: [],
+      msgText: '',
+    };
+  }
+  handleMsg = (e) => { 
+    this.setState({msgText: e.target.value});
+  };
+  sendMsg = (e) => {
+    socket.send(this.state.msgText);
+  };
+  render(){
+    return(
+    <>
+      <input type="textarea" value={this.state.msgText} onChange={this.handleMsg}/>
+      <button onClick={this.sendMsg}>Send message</button>
+    </>
+    );
+  }
 }
 
 export default App;
