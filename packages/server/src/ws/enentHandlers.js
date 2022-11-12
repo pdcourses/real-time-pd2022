ead281f8b670ede1befc35824b65272fad53592c
pdcourses <freshitpt@gmail.com>
2022-11-12 10:46:37 +0200

    server: message handler

--- packages/server/src/ws/enentHandlers.js
+++ packages/server/src/ws/enentHandlers.js
@@ -2,3 +2,15 @@ module.exports.testHandler = (data, options) => {
 	console.log('data = ', data);
 	console.log('options = ', options);
 };
+
+module.exports.messageHandler = (data, options) => {
+	console.log('data = ', data);
+	console.log('options = ', options);
+}
+
+/*
+module.exports.messageHandler = (room, message) => {
+	console.log('room = ', room);
+	console.log('message = ', message);
+}
+*/
